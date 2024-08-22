269d44d0e952f9f11ada7b328d291db276e211b6
Rubeniel <rubeniel74@gmail.com>
2024-08-22 14:53:43 -0300

    Pokemons List Resuelto

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,12 @@
-
-
-document.addEventListener("DOMContentLoaded", async () => {
+document.addEventListener("DOMContentLoaded", () => {
     const pokemonListElement = document.getElementById('pokemonList');
     const pokemonDetailsElement = document.getElementById('pokemon-details');
-    
+    const loadMoreButton = document.getElementById('loadMore');
 
+    let offset = 0;
+    const limit = 10;
+
+    // Convertir un Pokémon en un elemento de lista
     function convertPokemonToLi(pokemon) {
         const li = document.createElement('li');
         li.className = `pokemon ${pokemon.type}`;
@@ -13,13 +15,12 @@ document.addEventListener("DOMContentLoaded", async () => {
             <span class="name">${pokemon.name}</span>
             <div class="detail">
                 <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    ${pokemon.types.map(type => `<li class="type ${type}">${type}</li>`).join('')}
                 </ol>
                 <img src="${pokemon.photo}" alt="${pokemon.name}">
             </div>
         `;
 
-        
         li.addEventListener('click', () => {
             showPokemonDetails(pokemon);
         });
@@ -27,6 +28,7 @@ document.addEventListener("DOMContentLoaded", async () => {
         return li;
     }
 
+    // Mostrar detalles del Pokémon seleccionado
     function showPokemonDetails(pokemon) {
         pokemonDetailsElement.innerHTML = `
             <h2>${pokemon.name}</h2>
@@ -38,19 +40,28 @@ document.addEventListener("DOMContentLoaded", async () => {
         `;
     }
 
-    async function loadPokemonItens(offset, limit) {
-        const pokemons = await pokeApi.getPokemons(offset, limit);
-        pokemons.forEach(pokemon => {
-            const pokemonItem = convertPokemonToLi(pokemon);
-            pokemonListElement.appendChild(pokemonItem);
-        });
+    // Cargar una lista de Pokémon
+    async function loadPokemonItems() {
+        console.log(`Cargando Pokémon con offset ${offset}`);
+        try {
+            const pokemons = await pokeApi.getPokemons(offset, limit);
+            pokemons.forEach(pokemon => {
+                const pokemonItem = convertPokemonToLi(pokemon);
+                pokemonListElement.appendChild(pokemonItem);
+            });
+            offset += limit; // Actualizar el offset para la siguiente carga
+        } catch (error) {
+            console.error('Error cargando Pokémon:', error);
+        }
     }
 
-    const offset = 0;
-    const limit = 10;
-    loadPokemonItens(offset, limit);
+    // Cargar Pokémon inicial
+    loadPokemonItems();
 
-    
+    // Evento para el botón de "Cargar más"
+    loadMoreButton.addEventListener('click', () => {
+        loadPokemonItems();
+    });
 });
 
 
